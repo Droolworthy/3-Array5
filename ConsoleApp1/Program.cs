814c109e8e472e5c07022f7038fe5efc0168f894
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-07-18 20:00:06 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,7 +31,6 @@ namespace CS24
                 {
                     count = resetCounter;
                 }
-
                 if (numberRepetitions < count)
                 {
                     numberRepetitions = count;
