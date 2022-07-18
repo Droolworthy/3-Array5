ceb7faee81cfb4ac829099fc0fa9a54b5e042171
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-07-18 16:39:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CS24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[30];
+            Random random = new Random();
+            int maximumNumber = 1;
+            int count = 1;
+            int numberRepetitions = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] == array[i - 1])
+                {
+                    count++;
+                    if (count > maximumNumber)
+                    {
+                        maximumNumber = count;
+                        numberRepetitions = array[i];
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+
+            Console.WriteLine($"Число {numberRepetitions} повторяется большее число раз подряд");
+        }
+    }
+}
